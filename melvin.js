474af350e7a105e4595fc446041b377c03f973ed
PracticaFC <pprogra2023@gmail.com>
2025-02-13 12:06:13 -0600

    Add files via upload

---
+++ melvin.js
@@ -0,0 +1,28 @@
+function mostrarAnimalFavorito() {
+    document.getElementById("textoAnimal").innerText = "¡Mi animal favorito es el perro!";
+}
+
+
+function mostrarComidaFavorita() {
+    document.getElementById("textoComida").innerText = "Mi comida favorita es la pizza.";
+}
+
+
+function mostrarPeliculaFavorita() {
+    document.getElementById("textoPelicula").innerText = "Mi serie favorita es Breaking Bad.";
+}
+
+
+function mostrarCancionFavorita() {
+    document.getElementById("textoCancion").innerText = "Mi canción favorita es '5-7'.";
+}
+
+
+function mostrarPersonaAdmiro() {
+    document.getElementById("textoPersona").innerText = "Admiro a Albert Einstein por su genio y visión.";
+}
+
+
+function mostrarPaisVisitar() {
+    document.getElementById("textoPais").innerText = "Me gustaría visitar Japón por su cultura y tecnología.";
+}
